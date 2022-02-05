ab48ccf445518a79acc5eabc3f022e81bfb8adc9
shanks <cheikh.kebe.01@gmail.com>
2022-02-05 12:54:50 +0100

    cart setted ine application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
 
-  protected
+  before_action :current_cart
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  private
+
+  def current_cart
+    if user_signed_in?
+      if session[:cart_id]
+        cart = Cart.find_by(:id => session[:cart_id])
+        if cart.present?
+          @current_cart = cart
+        else
+          session[:cart_id] = nil
+        end
+      end
+
+      if session[:cart_id] == nil
+        @current_cart = Cart.create(user_id: current_user.id)
+        session[:cart_id] = @current_cart.id
+      end
+    end
   end
 end
