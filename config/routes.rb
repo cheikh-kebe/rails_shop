76bd245fbdd9197f627b50965bdba692add6c0e9
shanks <cheikh.kebe.01@gmail.com>
2022-02-09 22:22:33 +0100

    custom devise registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :orders, only: [:new, :create]
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   resources :users , :path => "utilisateur" do 
     resources :carts, :path => "panier"
