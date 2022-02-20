8b3e189cf8df1d8f60465dfa624a8a3c3a772328
shanks <cheikh.kebe.01@gmail.com>
2022-02-20 15:13:37 +0100

    approximative seed for comics, needs to be more clean

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-20.times do 
-  item = Item.create!(
-    title: Faker::Creature::Cat.breed,
-    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
-    price: Faker::Number.decimal(l_digits: 2),
-  )
+require "rest-client"
+require "digest"
+require 'json'
+
+Public_key = ENV["MARVEL_PUBLIC_API_KEY"]
+Private_key = ENV["MARVEL_PRIVATE_API_KEY"]
+number = Random.new
+@time_stamp = number.rand(100)
+@md5 = Digest::MD5.new
+@hash = @md5.update "#{@time_stamp}#{Private_key}#{Public_key}"
+
+def data_set
+  api_data = {
+    public: Public_key,
+    private: Private_key,
+    ts: @time_stamp,
+    hash: @hash,
+  }
+
+  series = RestClient.get("http://gateway.marvel.com/v1/public/series?ts=#{api_data[:ts]}&apikey=#{api_data[:public]}&hash=#{api_data[:hash]}")
+  series_array = JSON.parse(series)["data"]["results"]
+
+  comics = RestClient.get("http://gateway.marvel.com/v1/public/comics?ts=#{api_data[:ts]}&apikey=#{api_data[:public]}&hash=#{api_data[:hash]}")
+  comics_array = JSON.parse(comics)["data"]["results"]
+
+  series_array.map do |item|
+    Item.create!(
+      title: item["title"],
+      description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+      price: Faker::Number.decimal(l_digits: 2),
+      image_url: "#{item["thumbnail"]["path"] ? item["thumbnail"]["path"] : "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" }/portrait_uncanny.jpg"
+    )
+  end
+  comics_array.map do |item|
+    Item.create!(
+      title: item["title"],
+      description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+      price: Faker::Number.decimal(l_digits: 2),
+      image_url: "#{item["thumbnail"]["path"] ? item["thumbnail"]["path"] : "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" }/portrait_uncanny.jpg"
+    )
+  end
+  # variants = RestClient.get("http://gateway.marvel.com/v1/public/comics?ts=#{api_data[:ts]}&apikey=#{api_data[:public]}&hash=#{api_data[:hash]}")
+  # variants_array = JSON.parse(variants)["data"]["results"]["variants"]
+
+
 end
+
+data_set()
+
+# 20.times do
+#   item = Item.create!(
+#     title: Faker::Creature::Cat.breed,
+#     description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+#     price: Faker::Number.decimal(l_digits: 2),
+#   )
+# end
