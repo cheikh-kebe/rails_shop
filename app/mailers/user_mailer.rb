8dee5551959f1376c0ca288059eba913a3757faf
shanks <cheikh.kebe.01@gmail.com>
2022-03-09 13:13:32 +0100

    fix mailer smtp

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: 'http://localhost:3000'
+  default from: 'comicssho@mail.com'
 
   def welcome_email(user)
     @user = user
     @website = "Marvel Comics Shop"
     @url  = 'http://localhost:3000/users/sign_in'
-    mail(to: @user.email, subject: 'Bienvenue sur Marvel Comics Shop')
+    mail(to: user.email, subject: 'Bienvenue sur Marvel Comics Shop')
   end
 end
