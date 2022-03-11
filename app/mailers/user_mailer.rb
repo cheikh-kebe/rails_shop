a7f2131a2fe909c843f61a984c630d1290382cbf
shanks <cheikh.kebe.01@gmail.com>
2022-03-11 12:02:06 +0100

    fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'no-reply@marvel-comics-shop.herokuapp.com'
+  default from: 'https://marvel-comics-shop.herokuapp.com/'
 
   def welcome_email(user)
     @user = user
